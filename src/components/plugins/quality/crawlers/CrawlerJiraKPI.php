117e81d7d7cb218dd5846687eb547fd34f00c3ce
jeyroik <jeyroik@gmail.com>
2020-05-26 09:25:12 +0300

    added flow + rm container direct access

--- src/components/plugins/quality/crawlers/CrawlerJiraKPI.php
+++ src/components/plugins/quality/crawlers/CrawlerJiraKPI.php
@@ -3,22 +3,23 @@ namespace extas\components\plugins\quality\crawlers;
 
 use extas\components\quality\crawlers\Crawler;
 use extas\components\quality\crawlers\jira\kpi\rates\JiraKPIRate;
-use extas\components\SystemContainer;
 use extas\interfaces\quality\crawlers\ICrawler;
 use extas\interfaces\quality\crawlers\jira\control\rates\IJiraControlRate;
-use extas\interfaces\quality\crawlers\jira\control\rates\IJiraControlRateRepository;
 use extas\interfaces\quality\crawlers\jira\issues\rates\IJiraIssuesRate;
-use extas\interfaces\quality\crawlers\jira\issues\rates\IJiraIssuesRateRepository;
 use extas\interfaces\quality\crawlers\jira\kpi\rates\IJiraKPIRate;
 use extas\interfaces\quality\crawlers\jira\kpi\rates\IJiraKPIRateRepository;
 use extas\interfaces\quality\crawlers\jira\reactions\rates\IJiraReactionRate;
-use extas\interfaces\quality\crawlers\jira\reactions\rates\IJiraReactionRateRepository;
 use extas\interfaces\quality\indexes\IIndex;
-use extas\interfaces\quality\indexes\IIndexRepository;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Class CrawlerJiraKPI
+ *
+ * @method indexRepository()
+ * @method jiraControlRateRepository()
+ * @method jiraIssuesRateRepository()
+ * @method jiraReactionRateRepository()
+ * @method jiraKPIRateRepository()
  *
  * @package extas\components\plugins\quality\crawlers
  * @author jeyroik@gmail.com
@@ -35,12 +36,7 @@ class CrawlerJiraKPI extends Crawler
     public function __invoke(OutputInterface &$output): ICrawler
     {
         /**
-         * @var $qualificationRepo IIndexRepository
-         * @var $controlRepo IJiraControlRateRepository
-         * @var $issuesRepo IJiraIssuesRateRepository
-         * @var $reactionRepo IJiraReactionRateRepository
          * @var $kpiRepo IJiraKPIRateRepository
-         *
          * @var $qs IIndex[]
          * @var $control IJiraControlRate
          * @var $issuesRate IJiraIssuesRate
@@ -48,17 +44,12 @@ class CrawlerJiraKPI extends Crawler
          * @var $kpi IJiraKPIRate
          */
 
-        $qualificationRepo = SystemContainer::getItem(IIndexRepository::class);
-        $controlRepo = SystemContainer::getItem(IJiraControlRateRepository::class);
-        $issuesRepo = SystemContainer::getItem(IJiraIssuesRateRepository::class);
-        $reactionRepo = SystemContainer::getItem(IJiraReactionRateRepository::class);
-
         $month = (int) date('Ym');
 
-        $qs = $qualificationRepo->all([IIndex::FIELD__INDEX_MONTH => $month]);
-        $control = $controlRepo->one([IJiraControlRate::FIELD__MONTH => $month]);
-        $issuesRate = $issuesRepo->one([IJiraIssuesRate::FIELD__MONTH => $month]);
-        $reactionRate = $reactionRepo->one([IJiraReactionRate::FIELD__MONTH => $month]);
+        $qs = $this->indexRepository()->all([IIndex::FIELD__INDEX_MONTH => $month]);
+        $control = $this->jiraControlRateRepository()->one([IJiraControlRate::FIELD__MONTH => $month]);
+        $issuesRate = $this->jiraIssuesRateRepository()->one([IJiraIssuesRate::FIELD__MONTH => $month]);
+        $reactionRate = $this->jiraReactionRateRepository()->one([IJiraReactionRate::FIELD__MONTH => $month]);
 
         $qRate = 0;
 
@@ -73,7 +64,7 @@ class CrawlerJiraKPI extends Crawler
 
         $output->writeln(['<info>Rate = ' . $rate . '</info>']);
 
-        $kpiRepo = SystemContainer::getItem(IJiraKPIRateRepository::class);
+        $kpiRepo = $this->jiraKPIRateRepository();
         $kpi = $kpiRepo->one([IJiraKPIRate::FIELD__MONTH => $month]);
 
         if (!$kpi) {
